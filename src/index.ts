5e6d10916c571e3fa19ba3a107705f9792dd3f88
Youichi-Higa <yo1_1023@yahoo.co.jp>
2023-01-10 17:32:54 +0900

    feat: 初期画面の作成

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+
+import { createInitialPage } from '@/views/createInitialPage';
+
+const app = document.getElementById('app') as HTMLDivElement;
+const initialPage = createInitialPage();
+app.append(initialPage);
