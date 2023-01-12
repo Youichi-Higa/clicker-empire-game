090655f460f85316a97580ac0252ed213c7e205f
Youichi-Higa <yo1_1023@yahoo.co.jp>
2023-01-12 11:32:04 +0900

    chore: Font Awesome

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+
+import './style.css';
 
 import { createInitialPage } from '@/views/createInitialPage';
 
